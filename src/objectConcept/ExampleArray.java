579157a5650de3c898accdf34c1703402b56156b
Sagarika Sahoo <sagarika.sahoo@compassitesinc.com>
2018-11-29 18:29:07 +0530

    ArrayExample

---
+++ src/objectConcept/ExampleArray.java
@@ -0,0 +1,15 @@
+package objectConcept;
+
+public class ExampleArray {
+
+	public static void main(String[] args) {
+		int[] arr = new int[3];
+		
+		arr[0] =10;
+		arr[1] =20;
+		arr[2] =30;
+		System.out.println("Element index 1 is "+ arr[1]);
+
+	}
+
+}
